9eedae1912627e61f5ba3cd25b97289a3dbc76b3
trinidad cerri <trinidad.cerri@gmail.com>
2023-02-28 20:13:25 +0100

    watch list missing movies and style

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  # * /bookmarks/:id
+  resources :bookmarks, only: %i[destroy]
 end
